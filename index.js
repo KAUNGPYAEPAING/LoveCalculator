7193bf48296360dfb8238ba01abfe1da87ea2569
KAUNGPYAEPAING <88584127+KAUNGPYAEPAING@users.noreply.github.com>
2021-08-23 12:45:01 +0630

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+/****************Love calculator****************/    
+
+    var user = prompt("What is your name?");
+    var crush = prompt("What is your crush name?");
+
+    var lovescore = Math.random();
+    lovescore = lovescore * 100;
+    lovescore = Math.floor(n) + 1;
+    alert(user + " and " + crush + " will choose each others in the percentage of " + lovescore + "%");
