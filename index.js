2bf75e84d82155c1cdab52ccae567b2273939e08
KAUNGPYAEPAING <88584127+KAUNGPYAEPAING@users.noreply.github.com>
2021-08-23 14:57:41 +0630

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,15 @@
     var lovescore = Math.random();
     lovescore = lovescore * 100;
     lovescore = Math.floor(n) + 1;
-    alert(user + " and " + crush + " will choose each others in the percentage of " + lovescore + "%");
+
+    if(lovescore > 70){
+    alert(user + " and " + crush + " will choose each others in the percentage of " + lovescore + "%. I hope you guys will make each other happy." );
+    }
+
+    if(lovescore > 30 && lovescore <= 70){
+    alert(user + " and " + crush + " will choose each others in the percentage of " + lovescore + "%." );
+    }
+    
+    if(lovescore < 30){
+    alert(user + " and " + crush + " will choose each others in the percentage of " + lovescore + "%. You guys are like oil and water. But don't trust us completely, we can't be right all the time." );
+    }
